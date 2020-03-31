b10734a5bcc53c6dc34985a9810db22b3b93740d
matthew enbar <matthew.enbar@gmail.com>
2020-03-31 23:57:06 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,10 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
+
+#class constant - variable to all instnaces of a particular class
+# in the case the class is Book
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -8,6 +12,12 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+#getter for genre that also pushes the genre to the class constant GENRES
+  def genre= (genre)
+    @genre = genre
+    GENRES << genre
   end
 
 end
