7474ad47fcca25cad92ca374b58c5716fed24fe1
matthew enbar <matthew.enbar@gmail.com>
2020-03-31 23:39:24 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,6 +2,10 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+#class constant - variable to all instnaces of a particular class
+# in the case the class is Book 
+  GENRES = []
+
   def initialize(title)
     @title = title
   end
